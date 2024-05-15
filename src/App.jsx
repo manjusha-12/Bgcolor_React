50f4d78350f10e6eee1bd6d49957b7ad33be3df6
manjusha-12 <160860406+manjusha-12@users.noreply.github.com>
2024-05-15 11:17:56 +0530

    Bgcolor Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css'
+import './index.css'
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState('white');
@@ -16,7 +17,12 @@ function App() {
         <button style={{backgroundColor:'green',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('green')}>Green</button>
         <button style={{backgroundColor:'blue',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('blue')}>Blue</button>
         <button style={{backgroundColor:'yellow',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('yellow')}>Yellow</button>
+        <button style={{backgroundColor:'pink',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('pink')}>Pink</button>
+        <button style={{backgroundColor:'purple',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('purple')}>Purple</button>
+        <button style={{backgroundColor:'gray',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('gray')}>Gray</button>
+        <button style={{backgroundColor:'orange',borderRadius:'5px',boxShadow:'5px 3px 2px',margin:'10px'}} onClick={() => handleColorChange('orange')}>Orange</button>
      
+
     </div>
   );
 }
